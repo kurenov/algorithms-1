54d7d213127dabe6c5c15ee3c7c92ba40182aa2b
kurenov <olzhas.kurenov@gmail.com>
2016-12-24 19:55:51 +0600

    Added topological sort algorithm

---
+++ TopologicalSort.py
@@ -0,0 +1,8 @@
+###
+    # Code by Olzhas Kurenov
+    # Implementation of Topological Sort Algorithm
+    # Running time: O()
+###
+from Queue import Queue
+
+print '\n~~~~~~~~~Topological Sort Algorithm algorithm~~~~~~~~~\n'
