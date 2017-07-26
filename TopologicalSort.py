d3c072b04ca4eb257d34c455bfebe31368adbd97
kurenov <olzhas.kurenov@gmail.com>
2017-07-26 10:40:56 +0600

    Fixed Topological Sort

--- TopologicalSort.py
+++ TopologicalSort.py
@@ -1,42 +1,8 @@
 ###
     # Code by Olzhas Kurenov
     # Implementation of Topological Sort Algorithm
-    # Running time: O(n+m)
+    # Running time: O()
 ###
+from Queue import Queue
+
 print '\n~~~~~~~~~Topological Sort Algorithm algorithm~~~~~~~~~\n'
-
-graph = {};
-
-with open('acyclicGraph.txt') as f:
-    lines = f.readlines();
-
-for line in lines:
-    numbers = [int(x) for x in line.split()];
-    graph[numbers[0] - 1] = {
-        'visited': False,
-        'label': -1,
-        'edges': map(lambda v: v-1, numbers[1 : len(numbers)])
-    };
-
-n = len(lines);
-currentLabel = n - 1;
-print 'currentLabel', currentLabel;
-print graph;
-
-def DepthFirstSearch(g, s):
-    global currentLabel;
-    g[s]['visited'] = True;
-    for edge in g[s]['edges']:
-        if not g[edge]['visited']:
-            DepthFirstSearch(g, edge);
-    g[s]['label'] = currentLabel;
-    currentLabel = currentLabel - 1;
-
-def TopologicalSort(g):
-    for edge in g:
-        if not g[edge]['visited']:
-            DepthFirstSearch(g, edge);
-
-TopologicalSort(graph);
-
-print graph;
